650c9abf2e33c684dfecb74f13be88480e8564fb
Yair <yjgarizabalo@gmail.com>
2021-04-10 17:50:49 -0500

    [Yweb] update web home

---
+++ src/data/yweb_v.2.0.0/public/js/main.js
@@ -0,0 +1,15 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
