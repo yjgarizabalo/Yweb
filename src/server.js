4263be54bb225f2d405bb5d3a40f1f24373e95bb
Yair <yjgarizabalo@gmail.com>
2021-05-19 08:23:51 -0500

    [Yweb] add footer

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const { extname } = require('path')
 const path = require('path')
+const morgan = require('morgan')
 
 
 // Inicializaciones
@@ -19,6 +20,7 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 
 // Middlewares
+app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 
 // Variables globales
