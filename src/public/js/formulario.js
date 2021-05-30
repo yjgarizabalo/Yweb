d67b0572ea67f34c4c6761cce52674df59020b9e
Yair <yjgarizabalo@gmail.com>
2021-05-29 20:23:36 -0500

    [Yweb] add contacto

---
+++ src/public/js/formulario.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
